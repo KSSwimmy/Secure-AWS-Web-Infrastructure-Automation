54ea52bb26f065795b458d189c428ea46301f663
Kimberly Swinton <kimberlyswinton@gmail.com>
2026-07-15 23:22:15 -0400

    refactor: Update security group to use modern standalone rules

--- main.tf
+++ main.tf
@@ -1,39 +1,36 @@
 # 1. Security Group (Firewall) for the Web Server
 
+# 1. Create the Security Group (The empty firewall shell)
 resource "aws_security_group" "web_server_sg" {
   name        = "web-server-sg"
   description = "Allow HTTP and restricted SSH access"
 
-  # Ingress (Inbound) Rule: Allow standard Web Traffic (HTTP) from anyone on the internet
-  ingress {
-    description = "Allow HTTP"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"] 
-  }
-
-  # Ingress (Inbound) Rule: Allow Administrator SSH Access
-  ingress {
-    description = "Allow SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    # IMPORTANT: We are temporarily leaving this open to the world for testing. 
-    # Later, we will lock this down to ONLY your specific home IP address!
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  # Egress (Outbound) Rule: Allow the server to send traffic out to the internet
-  egress {
-    description = "Allow all outbound traffic"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1" # "-1" means all protocols
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   tags = {
     Name = "web-server-security-group"
   }
+}
+
+# 2. Inbound Rule: Allow standard Web Traffic (HTTP) from the internet
+resource "aws_vpc_security_group_ingress_rule" "allow_http" {
+  security_group_id = aws_security_group.web_server_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+
+# 3. Inbound Rule: Allow Administrator SSH Access
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
+  security_group_id = aws_security_group.web_server_sg.id
+  cidr_ipv4         = "0.0.0.0/0" # We will lock this down to your IP later!
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
+# 4. Outbound Rule: Allow the server to send traffic out to the internet
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.web_server_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # "-1" means all protocols
 }
